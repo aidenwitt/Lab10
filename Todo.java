5255b01700049404b9744f6c4895c87a3c42b5e0
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-04-16 22:36:25 -0500

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,52 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-04-16
+OU ID_______________: 113472850
+OU EMAIL____________: aidenwitt@ou.edu
+
+Tasks:
+1. Finish Polygon class
+2. Finish PolyLine class
+3. Finish Triangle class
+4. Finish RightTriangle class
+5. Finish IsocelesTriangle class
+6. Finish Rectangle class
+7. Finish Square class
+8. Finish Shape class
+9. Finish Circle class
+10. Finish Drawable class
+11. Finish Sinusoid class
+12. Finish DrawPanel class
+13. Finish DrawFrame class
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 10
+6. 10
+7. 10
+8. 10
+9. 10
+10. 10
+11. 10
+12. 10
+13. 10
+
+Actual Time (minutes):
+1. 30
+2. 20
+3. 10
+4. 11
+5. 9
+6. 8
+7. 5
+8. 7
+9. 9
+10. 12
+11. 5
+12. 8
+13. 30
+
+*/}
